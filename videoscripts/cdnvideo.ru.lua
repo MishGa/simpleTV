278031784845d4cc9479c24034cd26fea59b5a91
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-04 06:35:44 +0300

    Update cdnvideo.ru.lua

--- videoscripts/cdnvideo.ru.lua
+++ videoscripts/cdnvideo.ru.lua
@@ -1,9 +1,10 @@
--- видеоскрипт для https://cdnvideo.ru (29/9/20)
+-- видеоскрипт для https://cdnvideo.ru (4/10/20)
 -- Copyright © 2017-2020 Nexterr
 -- открывает подобные ссылки:
 -- https://player.cdnvideo.ru/iframer.html?id=7SwGd3mioH
+-- https://playercdn.cdnvideo.ru/aloha/players/uchalytv_player.html
 		if m_simpleTV.Control.ChangeAddress ~= 'No' then return end
-		if not m_simpleTV.Control.CurrentAddress:match('^https?://player%.cdnvideo%.ru') then return end
+		if not m_simpleTV.Control.CurrentAddress:match('^https?://player[%a]*%.cdnvideo%.ru') then return end
 	if m_simpleTV.Control.MainMode == 0 then
 		m_simpleTV.Interface.SetBackground({BackColor = 0, PictFileName = '', TypeBackColor = 0, UseLogo = 0, Once = 1})
 	end
@@ -17,7 +18,8 @@
 	m_simpleTV.Http.Close(session)
 		if rc ~= 200 then return end
 	answer = answer:gsub('%s', '')
-	local retAdr = answer:match('source:[\'"]([^\'"]+)')
+	local retAdr = answer:match('source:[\'"]([^\'"]+)') or answer:match('sourcesrc=[\'"]([^\'"]+)')
 		if not retAdr then return end
+	retAdr = retAdr:gsub('^//', 'http://')
 	m_simpleTV.Control.CurrentAddress = retAdr
 -- debug_in_file(retAdr .. '\n')
